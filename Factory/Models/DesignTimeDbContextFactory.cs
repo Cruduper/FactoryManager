0ca9e0e43e76186f5e76aa59075621f4c00064ce
Eric Crudup <briefcasemanx@gmail.com>
2022-03-18 09:32:56 -0700

    renamed fiprogram folder from 'FactoryManager' to 'Factory'

--- Factory/Models/DesignTimeDbContextFactory.cs
+++ Factory/Models/DesignTimeDbContextFactory.cs
@@ -3,23 +3,23 @@ using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
 using System.IO;
 
-namespace Registrar.Models
+namespace Factory.Models
 {
-  public class RegistrarContextFactory : IDesignTimeDbContextFactory<RegistrarContext>
+  public class FactoryContextFactory : IDesignTimeDbContextFactory<FactoryContext>
   {
 
-    RegistrarContext IDesignTimeDbContextFactory<RegistrarContext>.CreateDbContext(string[] args)
+    FactoryContext IDesignTimeDbContextFactory<FactoryContext>.CreateDbContext(string[] args)
     {
       IConfigurationRoot configuration = new ConfigurationBuilder()
           .SetBasePath(Directory.GetCurrentDirectory())
           .AddJsonFile("appsettings.json")
           .Build();
 
-      var builder = new DbContextOptionsBuilder<RegistrarContext>();
+      var builder = new DbContextOptionsBuilder<FactoryContext>();
 
       builder.UseMySql(configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(configuration["ConnectionStrings:DefaultConnection"]));
 
-      return new RegistrarContext(builder.Options);
+      return new FactoryContext(builder.Options);
     }
   }
 }
