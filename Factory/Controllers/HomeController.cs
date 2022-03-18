d515acbc4d38dfde87cc8ef2b05864be1f78d230
Eric Crudup <briefcasemanx@gmail.com>
2022-03-18 10:51:08 -0700

    WIP: adding functionality to display engineers/machines on splash page

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,13 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Factory.Controllers
 {
+
+  private readonly ToDoListContext _db;
   public class HomeController : Controller
   {
+
+    public HomeController( FactoryContext db)
+    {
+      _db = db;
+    }
 
     [HttpGet("/")]
     public ActionResult Index()
     {
+      _db.Machines.ToList();
+      _db.Engineers.ToList();
       return View();
     }
 
