8a99b65c511f0df567d0ae40e41faeb0dd395d11
Eric Crudup <briefcasemanx@gmail.com>
2022-03-19 15:33:47 -0700

    adds create routes to engineerscontroller

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -1,5 +1,6 @@
 using Factory.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,5 +31,27 @@ namespace Factory.Controllers
                         .FirstOrDefault(m => m.EngineerId == id);
       return View(model);
     }
+
+    public ActionResult Create()
+    {
+      ViewBag.MachineId = new SelectList(_db.Machines, "MachineId", "Name");
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Engineer eng, int MachineId)
+    {
+      _db.Engineers.Add(eng);
+      _db.SaveChanges();
+
+      if (MachineId != 0)
+      {
+        _db.EngineerMachines.Add(new EngineerMachine() { EngineerId = eng.EngineerId, MachineId = MachineId });
+        _db.SaveChanges();
+      }
+
+      return RedirectToAction("Index");
+    }
+
   }
 }
